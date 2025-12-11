93e445c6278ddda8be4c860fa83885e9b453d882
Abdullah Majeed <abdullahdgk2006@gmail.com>
2025-12-11 18:29:26 +0500

    Commit 7: Added Bullet class

--- Abdullah.py
+++ Abdullah.py
@@ -1,21 +1,21 @@
 # ----------------------
-# ENEMY CLASS
+# BULLET CLASS
 # ----------------------
-class Enemy(GameObject):
-    def __init__(self):
-        x_pos = random.randint(0, WIDTH-40)
-        super().__init__(x_pos, 0)
-        self._size = 40
-        self._speed = 3
+class Bullet(GameObject):
+    def __init__(self, x, y):
+        super().__init__(x, y)
+        self._width = 5
+        self._height = 15
+        self._speed = 7
 
     def move(self):
-        self._y += self._speed
+        self._y -= self._speed
 
     def draw(self):
-        pygame.draw.circle(screen, RED, (self._x+self._size//2, self._y+self._size//2), self._size//2)
+        pygame.draw.rect(screen, WHITE, (self._x, self._y, self._width, self._height))
 
     def off_screen(self):
-        return self._y > HEIGHT
+        return self._y < -10
 
     def get_rect(self):
-        return (self._x, self._y, self._size, self._size)
+        return (self._x, self._y, self._width, self._height)
