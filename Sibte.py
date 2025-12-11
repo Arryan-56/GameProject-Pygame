1839497f5ea1c818cbbee0c0f1ea59c1b4947b80
si87eali <nuevosiette@gmail.com>
2025-12-11 22:11:18 +0500

    Commit 9: added FPS

--- Sibte.py
+++ Sibte.py
@@ -5,3 +5,13 @@
 
 fps_text = font.render(f"FPS: {int(clock.get_fps())}", True, WHITE)
 screen.blit(fps_text, (10, 10))
+
+
+#=============================
+#===Commit 10: Map Borders====
+#=============================
+
+
+# Prevent player from leaving window as it creates map borders
+player_rect.x = max(0, min(player_rect.x, WIDTH - player_rect.width))
+player_rect.y = max(0, min(player_rect.y, HEIGHT - player_rect.height))
