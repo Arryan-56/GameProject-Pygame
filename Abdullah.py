0ff3e850e3ec213c15a229b7c428b2d185badd08
Abdullah Majeed <abdullahdgk2006@gmail.com>
2025-12-11 18:28:08 +0500

    Commit 6: Added Enemy class

--- Abdullah.py
+++ Abdullah.py
@@ -1,29 +1,21 @@
-class Player(GameObject):
+# ----------------------
+# ENEMY CLASS
+# ----------------------
+class Enemy(GameObject):
     def __init__(self):
-        super().__init__(WIDTH//2, HEIGHT-80)
-        self._size = 50
-        self._speed = 5
-        self._lives = 3
-        self.bullets_fired = 0
-        self.hits = 0
-        self.lives_lost = 0
-
-    def get_lives(self):
-        return self._lives
-
-    def reduce_life(self):
-        self._lives -= 1
-        self.lives_lost += 1
+        x_pos = random.randint(0, WIDTH-40)
+        super().__init__(x_pos, 0)
+        self._size = 40
+        self._speed = 3
 
     def move(self):
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_LEFT] and self._x > 0:
-            self._x -= self._speed
-        if keys[pygame.K_RIGHT] and self._x < WIDTH - self._size:
-            self._x += self._speed
+        self._y += self._speed
 
     def draw(self):
-        pygame.draw.rect(screen, WHITE, (self._x, self._y, self._size, self._size))
+        pygame.draw.circle(screen, RED, (self._x+self._size//2, self._y+self._size//2), self._size//2)
+
+    def off_screen(self):
+        return self._y > HEIGHT
 
     def get_rect(self):
         return (self._x, self._y, self._size, self._size)
